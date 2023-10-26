f100d1ddfa2398293e4281cceac83bc6ec503162
ArthurZanoelo <149025266+ArthurZanoelo@users.noreply.github.com>
2023-10-25 22:08:58 -0400

    Update Lab6Arthur.py

--- Lab6Arthur.py
+++ Lab6Arthur.py
@@ -4,20 +4,18 @@ def encoder():
     while i != len(password):
         password[i] = int(password[i])
         password[i] += 3
+        if password[i] == 10:
+            password[i] = 0
+        elif password[i] == 11:
+            password[i] = 1
+        elif password[i] == 12:
+            password[i] = 2
         password[i] = str(password[i])
         i += 1
     return ''.join(password)
 
 def decoder(encoded_password):
-    password = str(encoded_password)
-    decoded = ""
-    for i in range(0, 8):
-        value = int(password[i]) - 3
-        if value < 0:
-            value = 10 + value
-        decoded = decoded + str(value)
-    return decoded
-
+    pass
 
 def main():
     user_input = int(input('Please enter an option: '))
@@ -27,7 +25,7 @@ def main():
             encoded_password = encoder()
             print('Your password has been encoded and stored!')
         elif user_input == 2:
-            print("The encoded password is " + encoded_password + ", and the original password is " + decoder(encoded_password) +".")
+            decoder(encoded_password)
         user_input = int(input('Please enter an option: '))
 
 if __name__ == '__main__':
